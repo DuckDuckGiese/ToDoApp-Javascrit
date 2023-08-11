1ce242b3145dce9ca81fbc88742c635caa11fc97
Ricky Giese <richard.giese@icloud.com>
2023-08-11 17:19:35 -0500

    JS setup

---
+++ script.js
@@ -0,0 +1,11 @@
+const form = document.getElementById('form');
+const input = document.getElementById('input');
+
+form.addEventListener('submit', (e) => {
+
+    const todo = input.value;
+
+    if(todo) {
+        
+    }
+})
