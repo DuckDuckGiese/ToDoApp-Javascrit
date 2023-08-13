747390b7efb3a235064993c7cbed3c6995762bfb
Ricky Giese <richard.giese@icloud.com>
2023-08-13 17:39:24 -0500

    Finished

--- script.js
+++ script.js
@@ -1,22 +1,69 @@
 const form = document.getElementById("form");
 const input = document.getElementById("input");
-const todos = document.getElementById("todos");
+const todosUL = document.getElementById("todos");
+
+const todos = JSON.parse(localStorage.getItem("todos"));
+
+if (todos) {
+    todos.forEach((todo) => {
+        addTodo(todo);
+    });
+}
 
 form.addEventListener("submit", (e) => {
     e.preventDefault();
 
-    const todoText = input.value;
+    addTodo();
+});
 
-    if(todoText) {
-        const todoE1 = document.createElement("li");
-        todoE1.innerText = todoText;
+function addTodo(todo) {
+    let todoText = input.value;
 
-        todoE1.addEventListener('click', () => {
-            todoE1.classList.toggle('completed')
+    if (todo) {
+        todoText = todo.text;
+    }
+
+    if (todoText) {
+        const todoEl = document.createElement("li");
+        if (todo && todo.completed) {
+            todoEl.classList.add("completed");
+        }
+
+        todoEl.innerText = todoText;
+
+        todoEl.addEventListener("click", () => {
+            todoEl.classList.toggle("completed");
+
+            updateLS();
         });
 
-        todos.appendChild(todoE1);
+        todoEl.addEventListener("contextmenu", (e) => {
+            e.preventDefault();
+
+            todoEl.remove();
+
+            updateLS();
+        });
+
+        todosUL.appendChild(todoEl);
 
         input.value = "";
+
+        updateLS();
     }
-})
+}
+
+function updateLS() {
+    const todosEl = document.querySelectorAll("li");
+
+    const todos = [];
+
+    todosEl.forEach((todoEl) => {
+        todos.push({
+            text: todoEl.innerText,
+            completed: todoEl.classList.contains("completed"),
+        });
+    });
+
+    localStorage.setItem("todos", JSON.stringify(todos));
+}
